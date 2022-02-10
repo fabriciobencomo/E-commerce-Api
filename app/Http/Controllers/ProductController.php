4b364d57b69b97500487a5b95bfb36ac5fd35697
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-02-10 14:50:13 -0400

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function __construct() {
+
+        $this->middleware("auth:sanctum")
+                ->except(["index", "show"]);
+
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return $products;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProductRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProductRequest $request)
+    {
+        $product = Product::create($request->all());
+        return $product;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        $product = new ProductResource($product);
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProductRequest  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json();
+    }
+}
---
+++ tests/Feature/ProductControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Product;
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Sanctum::actingAs(User::factory()->create());
+    }
+
+    public function test_index()
+    {
+        $product = Product::factory()->count(5)->create();
+
+        $response = $this->getJson('/api/products');
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(5);
+    }
+
+    public function test_create_new_product()
+    {
+        $data = [
+            'name' => 'Hola',
+            'price' => 1000,
+        ];
+        $response = $this->postJson('/api/products', $data);
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $this->assertDatabaseHas('products', $data);
+    }
+
+    public function test_update_product()
+    {
+        /** @var Product $product */
+        $product = Product::factory()->create();
+
+        $data = [
+            'name' => 'Update Product',
+            'price' => 20000,
+        ];
+
+        $response = $this->patchJson("/api/products/{$product->getKey()}", $data);
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+    }
+
+    public function test_show_product()
+    {
+        /** @var Product $product */
+        $product = Product::factory()->create();
+
+        $response = $this->getJson("/api/products/{$product->getKey()}");
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+    }
+
+    public function test_delete_product()
+    {
+        /** @var Product $product */
+        $product = Product::factory()->create();
+
+        $response = $this->deleteJson("/api/products/{$product->getKey()}");
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $this->assertDatabaseMissing('products',$product->id);
+    }
+}
